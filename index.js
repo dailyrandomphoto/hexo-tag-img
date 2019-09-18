276eefcf2fec51dd22abf1f3feaeb1db1c246822
dailyrandomphoto <dailyrandomphoto@gmail.com>
2019-09-18 20:25:54 +0800

    set up as a hexo plugin

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 'use strict';
 
-module.exports = ctx => {
-  const { tag } = ctx.extend;
-
-  const img = require('./lib/img')(ctx);
-
-  tag.register('img', img);
-  tag.register('image', img);
-};
+require('./lib')(hexo);
